43f4ecde980390b8e7fadb04d62f9518b65776e2
Abishazaigo <you@example.com>
2021-02-04 18:58:54 +0530

    Component created for login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+function Login(){
+    return(
+        <div className="bg-gradient-primary sidebar-toggled">
+            <div className="container">
+               <
+            </div>
+        </div>
+    )
+}
+export default Login;
