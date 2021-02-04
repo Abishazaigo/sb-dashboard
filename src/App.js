a27810492cbb42f64224401f9a4969750f9684f4
Abishazaigo <you@example.com>
2021-02-04 18:59:49 +0530

    Added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-
+import './Styles/Sb.css';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact="/">
+            <Login />
+          </Route>
+          <Route exact="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
