cfbb3072ea364f8beac7c26e12af34b0708718f5
Abishazaigo <you@example.com>
2021-02-08 18:40:28 +0530

    Connected with router and query selector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {useAlert, positions, Provider as AlertProvider} from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import './Styles/Db.css';
 import './Styles/Sb.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Email from './components/Email';
+import Form from './components/Form';
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className="app">
+    <QueryClientProvider client={queryClient}>
       <Router>
-        <Switch>
-          <Route exact="/">
-            <Login />
-          </Route>
-          <Route exact="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
+        <Route>
+            <Email />
+        </Route>
       </Router>
-    </div>
+    </QueryClientProvider>
   );
 }
 
