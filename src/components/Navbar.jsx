dbdeef495b63ff8ced193e7a4b2b870354b77438
Abishazaigo <you@example.com>
2021-02-09 18:41:17 +0530

    Component created for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 function Navbar(props){
     return(
         <li className="nav-item">
-            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target={props.target} aria-expanded="true" aria-controls={props.className}>
                 <i className={props.icon}></i>
                 <span>{props.topic}</span>
             </a>
-            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div id={props.id} className="collapse" aria-labelledby={props.label} data-parent="#accordionSidebar">
                 <div className="bg-white py-2 collapse-inner rounded">
                     {props.children}
                 </div>
