f378ba7073d03cccb8dbe92f45ce9a544726e373
Abishazaigo <you@example.com>
2021-02-09 09:30:26 +0530

    Connected with email component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import './Styles/Db.css';
-import './Styles/Sb.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Email from './components/Email';
-import Form from './components/Form';
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <Route>
-            <Email />
-        </Route>
+        <Switch>
+          <Email />
+        </Switch>
       </Router>
     </QueryClientProvider>
   );
