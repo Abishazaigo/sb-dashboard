f66b1c9fcffa8e4f5a8e927d0b275027e6d85dbe
Abishazaigo <you@example.com>
2021-02-09 11:34:28 +0530

    Component created for dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,282 @@
+import Navbar from '../components/Navbar';
+import Interface from './Interface';
+import Dropdown from './Dropdown';
+import Card from './Card';
+import Post from './Post';
+import '../Styles/Db.css';
+function Dashboard(){
+    let date=new Date();
+    let year=date.getFullYear();
+    return(
+        <div className="page-top" id="page-top">
+            <div className="wrapper" id="wrapper">
+                <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <div className="sidebar-brand-icon rotate-n-15">
+                            <i className="fas fa-laugh-wink"></i>
+                        </div>
+                        <div className="sidebar-brand-text mx-3">SB Admin</div>
+                    </a>
+                    <hr className="sidebar-divider my-0" />
+                    <li className="nav-item active">
+                        <a className="nav-link" href="index.html">
+                            <i className="fas fa-fw fa-tachometer-alt"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <hr className="sidebar-divider" />
+                    <div className="sidebar-heading">
+                        Interface
+                    </div>
+                    <Navbar icon="fas fa-fw fa-cog" topic="Components" subtopic="Custom Components:">
+                        <Interface link="buttons.html" label="Buttons" />
+                        <Interface link="cards.html" label="Cards" />
+                    </Navbar> 
+                    <Navbar icon="fas fa-fw fa-wrench" topic="Utilities" subtopic="Custom Utilities">
+                        <Interface link="utilities-color.html" label="Colors" />
+                        <Interface link="utilities-border.html" label="Borders" />
+                        <Interface link="utilities-animation.html" label="Animations" />
+                        <Interface link="utilities-other.html" label="Other" />
+                    </Navbar>
+                    <hr className="sidebar-divider d-none d-md-block" /> 
+                    <div className="text-center d-none d-md-inline">
+                        <button className="rounded-circle border-0" id="sidebarToggle"></button>
+                    </div>
+                    <div className="sidebar-card">
+                        <img className="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="" />
+                        <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
+                        <a className="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
+                    </div>
+                </ul>
+        <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+                <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item dropdown no-arrow mx-1">
+                            <a className="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i className="fas fa-bell fa-fw"></i>
+                                <span className="badge badge-danger badge-counter">3+</span>
+                            </a>
+                            <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                                <h6 className="dropdown-header">
+                                    Alerts Center
+                                </h6>
+                                <Dropdown className="icon-circle bg-primary" icon="fas fa-file-alt text-white" label="December 12, 2019" content="A new monthly report is ready to download!" />
+                                <Dropdown className="icon-circle bg-success" icon="fas fa-donate text-white" label="December 7, 2019" content="$290.29 has been deposited into your account!" />
+                                <Dropdown className="icon-circle bg-warning" icon="fas fa-exclamation-triangle text-white" label="December 2, 2019" content="Spending Alert: We've noticed unusually high spending for your account." />
+                                <a className="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                            </div>
+                        </li>
+                        <li className="nav-item dropdown no-arrow mx-1">
+                            <a className="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i className="fas fa-envelope fa-fw"></i>
+                                <span className="badge badge-danger badge-counter">7</span>
+                            </a>
+                            <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
+                                <h6 className="dropdown-header">
+                                    Message Center
+                                </h6>
+                                <a className="dropdown-item d-flex align-items-center" href="#">
+                                    <div className="dropdown-list-image mr-3">
+                                        <img className="rounded-circle" src="img/undraw_profile_1.svg" alt="" />
+                                        <div className="status-indicator bg-success"></div>
+                                    </div>
+                                    <div className="font-weight-bold">
+                                        <div className="text-truncate">Hi there! I am wondering if you can help me with a
+                                            problem I've been having.</div>
+                                        <div className="small text-gray-500">Emily Fowler · 58m</div>
+                                    </div>
+                                </a>
+                                <a className="dropdown-item d-flex align-items-center" href="#">
+                                    <div className="dropdown-list-image mr-3">
+                                        <img className="rounded-circle" src="img/undraw_profile_2.svg" alt="" />
+                                        <div className="status-indicator"></div>
+                                    </div>
+                                    <div>
+                                        <div className="text-truncate">I have the photos that you ordered last month, how
+                                            would you like them sent to you?</div>
+                                        <div className="small text-gray-500">Jae Chun · 1d</div>
+                                    </div>
+                                </a>
+                                <a className="dropdown-item d-flex align-items-center" href="#">
+                                    <div className="dropdown-list-image mr-3">
+                                        <img className="rounded-circle" src="img/undraw_profile_3.svg" alt="" />
+                                        <div className="status-indicator bg-warning"></div>
+                                    </div>
+                                    <div>
+                                        <div className="text-truncate">Last month's report looks great, I am very happy with
+                                            the progress so far, keep up the good work!</div>
+                                        <div className="small text-gray-500">Morgan Alvarez · 2d</div>
+                                    </div>
+                                </a>
+                                <a className="dropdown-item d-flex align-items-center" href="#">
+                                    <div className="dropdown-list-image mr-3">
+                                        <img className="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="" />
+                                        <div className="status-indicator bg-success"></div>
+                                    </div>
+                                    <div>
+                                        <div className="text-truncate">Am I a good boy? The reason I ask is because someone
+                                            told me that people say this to all dogs, even if they aren't good...</div>
+                                        <div className="small text-gray-500">Chicken the Dog · 2w</div>
+                                    </div>
+                                </a>
+                                <a className="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                            </div>
+                        </li>
+                    <div className="topbar-divider d-none d-sm-block"></div>
+                        <li className="nav-item dropdown no-arrow">
+                            <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span className="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <img className="img-profile rounded-circle" src="img/undraw_profile.svg" />
+                            </a>
+                            <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a className="dropdown-item" href="#">
+                                    <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Profile
+                                </a>
+                                <a className="dropdown-item" href="#">
+                                    <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Settings
+                                </a>
+                                <a className="dropdown-item" href="#">
+                                    <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Activity Log
+                                </a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="container-fluid">
+                    <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                    </div>
+                    <div className="row">
+                        <div className="col-xl-3 col-md-6 mb-4">
+                            <div className="card border-left-primary shadow h-100 py-2">
+                                <div className="card-body">
+                                    <div className="row no-gutters align-items-center">
+                                        <div className="col mr-2">
+                                            <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                Earnings (Monthly)</div>
+                                            <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                        </div>
+                                        <div className="col-auto">
+                                            <i className="fas fa-calendar fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-md-6 mb-4">
+                            <div className="card border-left-success shadow h-100 py-2">
+                                <div className="card-body">
+                                    <div className="row no-gutters align-items-center">
+                                        <div className="col mr-2">
+                                            <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Earnings (Annual)</div>
+                                            <div className="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                        </div>
+                                        <div className="col-auto">
+                                            <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-md-6 mb-4">
+                            <div className="card border-left-info shadow h-100 py-2">
+                                <div className="card-body">
+                                    <div className="row no-gutters align-items-center">
+                                        <div className="col mr-2">
+                                            <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            </div>
+                                            <div className="row no-gutters align-items-center">
+                                                <div className="col-auto">
+                                                    <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                </div>
+                                                <div className="col">
+                                                    <div className="progress progress-sm mr-2">
+                                                        <div className="progress-bar bg-info" role="progressbar"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-auto">
+                                            <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-3 col-md-6 mb-4">
+                            <div className="card border-left-warning shadow h-100 py-2">
+                                <div className="card-body">
+                                    <div className="row no-gutters align-items-center">
+                                        <div className="col mr-2">
+                                            <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Pending Requests</div>
+                                            <div className="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                        </div>
+                                        <div className="col-auto">
+                                            <i className="fas fa-comments fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-6 mb-4">
+                            <div className="row">
+                                <Post className="card bg-primary text-white shadow" background="Primary" text="text-white-50 small" color="#4e73df" />
+                                <Post className="card bg-success text-white shadow" background="Success" text="text-white-50 small" color="#1cc88a" />
+                                <Post className="card bg-info text-white shadow" background="Info" text="text-white-50 small" color="#36b9cc" />
+                                <Post className="card bg-warning text-white shadow" background="Warning" text="text-white-50 small" color="#f6c23e" />
+                                <Post className="card bg-danger text-white shadow" background="Danger" text="text-white-50 small" color="#e74a3b" />
+                                <Post className="card bg-secondary text-white shadow" background="Secondary" text="text-white-50 small" color="#858796" />
+                                <Post className="card bg-light text-black shadow" background="Light" text="text-black-50 small" color="#f8f9fc" />
+                                <Post className="card bg-dark text-white shadow" background="Dark" text="text-white-50 small" color="#5a5c69" />
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <footer className="sticky-footer bg-white">
+                <div className="container my-auto">
+                    <div className="copyright text-center my-auto">
+                        <span>Copyright © Your Website {year}</span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
+    </a>
+    <div className="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div className="modal-footer">
+                    <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a className="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    )
+}
+export default Dashboard;
