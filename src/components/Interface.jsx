92f5a5fe0e002bf29cd0214eb687e89f4bd8f9fe
Abishazaigo <you@example.com>
2021-02-09 18:40:33 +0530

    Component created for anchor tag

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,6 +1,8 @@
 function Interface(props){
     return(
-        <a className="collapse-item" href={props.link}>{props.label}</a>
+        <a className={props.className} href={props.link} data-toggle={props.data} data-target={props.target}>
+            <i className={props.icon}></i>
+        {props.label}</a>
     )
 }
 export default Interface;
