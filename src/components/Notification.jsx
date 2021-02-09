73f05e1b4fcf988fbc33aedb6076d128590fceb4
Abishazaigo <you@example.com>
2021-02-09 18:43:19 +0530

    Component created for notifications

---
+++ src/components/Notification.jsx
@@ -0,0 +1,16 @@
+function Notification(props){
+    return(
+        <a className="dropdown-item d-flex align-items-center" href="#">
+            <div className="dropdown-list-image mr-3">
+                <img className="rounded-circle" src={props.picture} alt="" />
+                <div className={props.className}>
+                </div>
+            </div>
+            <div className="text-truncate">{props.content}
+            </div>
+            <div className="small text-gray-500">{props.name}
+            </div>
+        </a>
+    )
+}
+export default Notification;
