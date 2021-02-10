31fbb15aeda36f9b1de1035d7907ed74fdff6027
Abishazaigo <you@example.com>
2021-02-10 18:49:21 +0530

    Component created for login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,9 @@
 import Form from './Form';
-import Label from './Label';
 function Login(){
     return(
         <div className="bg-gradient">
             <div className="bg-gradient-primary sidebar-toggled">
-                <div className="container">
+                <div className="topic">
                     <div className="row justify-content-center">
                         <div className="col-xl-10 col-lg-12 col-md-9">
                             <div className="card o-hidden border-0 shadow-lg my-5">
@@ -17,9 +16,7 @@ function Login(){
                                                         <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                                     </div>
                                                     <Form />
-                                                    <Label className="small" link="forgot-password.html" name="Forgot Password?" />
-                                                    <Label className="small" link="register.html" name="Create an Account!" />
-                                                </div>
+                                                    </div>
                                             </div>
                                     </div>
                                 </div>
