2c069152480fea3fd4af760bbcc6e1a1e0ca2831
Abishazaigo <you@example.com>
2021-02-10 18:51:47 +0530

    Component created for comments posted

---
+++ src/components/PostComment.jsx
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import axios from 'axios';
+function PostComment(){
+    const[comments, setComments]=useState({});
+    let {id} = useParams();
+    const url="https://jsonplaceholder.typicode.com/posts/"+id+"/comments";
+    useEffect(() => {
+        axios.get(url)
+          .then((res) => {
+            setComments(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
+    return(
+        <>
+        {comments && comments.length && comments.map((comment) => {
+            return(        
+            <div className="row">
+                <div className="title">{comment.email}</div>
+                <div className="content">{comment.body}</div>
+            </div>
+            );
+            })}
+        </>
+    )
+}
+export default PostComment;
