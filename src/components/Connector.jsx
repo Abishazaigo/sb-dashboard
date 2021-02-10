c7c704b1d648385824025584aaeddd57bcb61ce3
Abishazaigo <you@example.com>
2021-02-10 18:51:06 +0530

    Component created for links

---
+++ src/components/Connector.jsx
@@ -0,0 +1,8 @@
+function Connector(props){
+    return(
+        <a className={props.className} href={props.link} data-toggle={props.data} data-target={props.target}>
+            <i className={props.icon}></i>
+        {props.label}</a>
+    )
+}
+export default Connector;
