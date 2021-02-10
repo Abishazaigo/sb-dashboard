9a486a7d4bfeb2132442acf294bf1ae36511eeb5
Abishazaigo <you@example.com>
2021-02-10 18:48:56 +0530

    Component created for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom';
 import Navbar from './Navbar';
-import Interface from './Interface';
+import Connector from './Connector';
 import Gallery from './Gallery';
 import Button from './Button';
 function Header(){
     return(
-        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
+        <>
+        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled " id="accordionSidebar">
                     <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                         <div className="sidebar-brand-icon rotate-n-15">
                             <i className="fas fa-laugh-wink"></i>
@@ -22,27 +24,33 @@ function Header(){
                     <div className="sidebar-heading">
                         Interface
                     </div>
-                    <Navbar icon="fas fa-fw fa-cog" topic="Components" subtopic="Custom Components:" id="collapseTwo" className="collapseTwo" label="collapseTwo" target="#collapseTwo">
-                        <Interface className="collapse-item" link="buttons.html" label="Buttons" />
-                        <Interface className="collapse-item" link="cards.html" label="Cards" />
+                    <Navbar icon="fas fa-fw fa-cog" topic="Components" id="collapseTwo" className="collapseTwo" label="collapseTwo" target="#collapseTwo">
+                        <h6 className="collapse-header">Custom Components:</h6>
+                        <Connector className="collapse-item" link="buttons.html" label="Buttons" />
+                        <Link to="/posts">
+                        <Connector className="collapse-item" link="cards.html" label="Cards" />
+                        </Link>
                     </Navbar> 
-                    <Navbar icon="fas fa-fw fa-wrench" topic="Utilities" subtopic="Custom Utilities:" id="collapseUtilities" className="collapseUtilities" label="collapseUtilities" target="#collapseUtilities">
-                        <Interface className="collapse-item" link="utilities-color.html" label="Colors" />
-                        <Interface className="collapse-item" link="utilities-border.html" label="Borders" />
-                        <Interface className="collapse-item" link="utilities-animation.html" label="Animations" />
-                        <Interface className="collapse-item" link="utilities-other.html" label="Other" />
+                    <Navbar icon="fas fa-fw fa-wrench" topic="Utilities" id="collapseUtilities" className="collapseUtilities" label="collapseUtilities" target="#collapseUtilities">
+                        <h6 className="collapse-header">Custom Utilities:</h6>
+                        <Connector className="collapse-item" link="utilities-color.html" label="Colors" />
+                        <Connector className="collapse-item" link="utilities-border.html" label="Borders" />
+                        <Connector className="collapse-item" link="utilities-animation.html" label="Animations" />
+                        <Connector className="collapse-item" link="utilities-other.html" label="Other" />
                     </Navbar>
                     <div className="sidebar-heading">
                         Addons
                     </div>
                     <Navbar icon="fas fa-fw fa-folder" topic="Pages" id="collapsePages" className="collapsePages" label="collapsePages" target="#collapsePages">
                         <h6 className="collapse-header">Login screens:</h6>
-                        <Interface className="collapse-item" link="register.html" label="Register" />
-                        <Interface className="collapse-item" link="forgot-password.html" label="Forgot Password" />
+                        <Link to="/register">
+                        <Connector className="collapse-item" link="register.html" label="Register" />
+                        </Link>
+                        <Connector className="collapse-item" link="forgot-password.html" label="Forgot Password" />
                         <div className="collapse-divider"></div>
                         <h6 className="collapse-header">Other Pages:</h6>
-                        <Interface className="collapse-item" link="404.html" label="404 Page" />
-                        <Interface className="collapse-item" link="blank.html" label="Blank Page" />
+                        <Connector className="collapse-item" link="404.html" label="404 Page" />
+                        <Connector className="collapse-item" link="blank.html" label="Blank Page" />
                     </Navbar>
                     <hr className="sidebar-divider d-none d-md-block" /> 
                     <div className="text-center d-none d-md-inline">
@@ -51,9 +59,10 @@ function Header(){
                     <div className="sidebar-card">
                         <Gallery className="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="" />
                         <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                        <Interface className="btn btn-success btn-sm" link="https://startbootstrap.com/theme/sb-admin-pro" label="Upgrade to Pro!" />
+                        <Connector className="btn btn-success btn-sm" link="https://startbootstrap.com/theme/sb-admin-pro" label="Upgrade to Pro!" />
                     </div>
             </ul>
+            </>
     )
 }
 export default Header;
