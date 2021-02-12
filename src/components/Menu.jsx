2272f552af7e9d7972cbcd719ef5443a12647c8a
Abishazaigo <you@example.com>
2021-02-12 11:44:08 +0530

    Component created for the menu in the sidebar

---
+++ src/components/Menu.jsx
@@ -0,0 +1,8 @@
+function Menu(props){
+    return(
+        <a className={props.className} data-toggle={props.data} data-target={props.target}>
+            <i className={props.icon}></i>
+        {props.label}</a>
+    )
+}
+export default Menu;
